ae28d405181a3ab5f1d2c4b3bf94dad3aa127593
cndaniel <tigerljt@gmail.com>
2016-11-06 23:57:42 +0800

    fix front end design

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,8 +5,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
- # storage :file
-  storage :fog
+  #storage :file
+  # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -14,7 +14,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   # end
 
-  process resize_to_fit: [800, 800]
+  process resize_to_fit: [1200, 1200]
 
   version :thumb do
     process resize_to_fill: [200, 200]
